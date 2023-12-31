dc08047c690e858f0ca44c1a5d577feb4019ef50
varshini2-lang <145186068+varshini2-lang@users.noreply.github.com>
2023-12-31 20:21:19 +0530

    Create app.js

---
+++ frontend/src/app.js
@@ -0,0 +1,13 @@
+// App.js
+import React from "react";
+import LogQuery from "./LogQuery";
+
+function App() {
+  return (
+    <div className="App">
+      <LogQuery />
+    </div>
+  );
+}
+
+export default App;
